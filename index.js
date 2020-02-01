baab8b377e3ceb1131129f3b4ae95ce6a28b814a
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-02-01 06:37:57 -0500

    connected to socket

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
-const socket = require("socket.io");
 
 dotenv.config();
 connectDB();
-const app = express();
-const server = require("http").Server(app);
-const authRouter = require("./auth/auth-router.js");
 
-const PORT = process.env.PORT || 8000;
+// const authRouter = require("./auth/auth-router.js");
+
+const PORT = 8000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRouter);
+// app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: process.env.MESSAGE || "deployed" });
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
 });
-
-const io = socket(server);
-
+const io = require("socket.io")(server);
 io.on("connection", socket => {
   console.log(`socket is on !`);
   socket.emit("welcome back", { msg: "welcome back" });
